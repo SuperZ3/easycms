3810042d76ac5c48511d592fde6173364f656518
Super ZZZ <zhangzz1024@163.com>
2020-06-14 01:40:30 +0800

    Updates

---
+++ precache-manifest.5d88eb3240fd3a60b1b5e5f9030a2e2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2368780d69369de3addfb5614fe9222d",
+    "url": "/easycms/index.html"
+  },
+  {
+    "revision": "25fb5cf7a1f41f520fe0",
+    "url": "/easycms/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "ba04c52bed80b3af1290",
+    "url": "/easycms/static/css/main.e0ec0846.chunk.css"
+  },
+  {
+    "revision": "25fb5cf7a1f41f520fe0",
+    "url": "/easycms/static/js/2.8220dede.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/easycms/static/js/2.8220dede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba04c52bed80b3af1290",
+    "url": "/easycms/static/js/main.764fcc96.chunk.js"
+  },
+  {
+    "revision": "3aa64dc42b4a17caab8e",
+    "url": "/easycms/static/js/runtime-main.2b6f705f.js"
+  },
+  {
+    "revision": "44f9e3067c8b360837162eff63c61869",
+    "url": "/easycms/static/media/login.44f9e306.png"
+  }
+]);
